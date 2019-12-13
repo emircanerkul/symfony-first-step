43ada05bd7ea6daf3dc6701daea332345d7c353b
emircanerkul <develop@emircanerkul.com>
2019-12-13 22:33:36 +0300

    Currency | Cache

--- src/Service/Currency.php
+++ src/Service/Currency.php
@@ -2,24 +2,35 @@
 
 namespace App\Service;
 
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class Currency
 {
     private $currency_commission;
-
+    private $cache;
     public function __construct(ContainerInterface $containerInterface)
     {
         $this->currency_commission = $containerInterface->getParameter("currency_commission");
+        $this->cache = new FilesystemAdapter();
     }
 
     public function getEUR()
     {
-        return json_decode(file_get_contents("https://api.exchangeratesapi.io/latest?symbols=TRY&base=EUR"))->rates->TRY * (1 + $this->currency_commission);
+        return $this->cache->get("currency_eur", function (ItemInterface $item) {
+            $item->expiresAfter(60);
+
+            return json_decode(file_get_contents("https://api.exchangeratesapi.io/latest?symbols=TRY&base=EUR"))->rates->TRY * (1 + $this->currency_commission);
+        });
     }
 
     public function getUSD()
     {
-        return json_decode(file_get_contents("https://api.exchangeratesapi.io/latest?symbols=TRY&base=USD"))->rates->TRY * (1 + $this->currency_commission);
+        return $this->cache->get("currency_usd", function (ItemInterface $item) {
+            $item->expiresAfter(60);
+
+            return json_decode(file_get_contents("https://api.exchangeratesapi.io/latest?symbols=TRY&base=USD"))->rates->TRY * (1 + $this->currency_commission);
+        });
     }
 }
